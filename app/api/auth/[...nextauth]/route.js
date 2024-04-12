749611b09d8e1cc5a8c04cbfebc0883d32b0595b
Somondas <somondas067@gmail.com>
2024-04-12 19:30:10 +0530

    Updates

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
 import NextAuth from "next-auth/next";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
 const authOptions = {
   providers: [
@@ -7,13 +10,34 @@ const authOptions = {
       name: "credentials",
       credentials: {},
       async authorize(credentials) {
-        const user = { id: 1 };
-        return user;
+        const { email, password } = credentials;
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return null;
+          }
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (!passwordMatch) {
+            return null;
+          }
+          return user;
+        } catch (error) {
+          console.log(error);
+        }
       },
     }),
   ],
   session: {
     strategy: "jwt",
-    // secret:
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
   },
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,6 +1,7 @@
 import RegisterForm from "@/components/RegisterForm";
 import React from "react";
-
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 const Register = () => {
   return <RegisterForm />;
 };
