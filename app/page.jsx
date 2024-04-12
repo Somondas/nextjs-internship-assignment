7ae133d1d4a1bb8ef275cf724b12c7fdd11c58b6
Somondas <somondas067@gmail.com>
2024-04-12 19:39:16 +0530

    updates

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,13 @@
 import LoginForm from "@/components/LoginForm";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/dashboard");
+  }
   return <LoginForm />;
 }
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -2,7 +2,12 @@ import RegisterForm from "@/components/RegisterForm";
 import React from "react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-const Register = () => {
+import { authOptions } from "../api/auth/[...nextauth]/route";
+const Register = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/dashboard");
+  }
   return <RegisterForm />;
 };
 
